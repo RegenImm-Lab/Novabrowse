83c8e1196b175c1863fadcfb6ad2e0d4fc172700
Andreas Kähäri <andreas.kahari@nbis.se>
2026-01-28 12:28:10 +0100

    feat: Make cache configurable using ENTREZ_CACHE_EXPIRY_DAYS

--- docker/ncbi_cache-head.py
+++ docker/ncbi_cache-head.py
@@ -1,8 +1,13 @@
+import os
 import ncbi_cache
 
+cache_days = int(os.getenv("ENTREZ_CACHE_EXPIRY_DAYS", "3"))
+
 ncbi_cache.init_cache(
-    cache_dir="ncbi_cache", cache_duration_days=7, enabled=True
-).clear_cache(older_than_days=7)
+    cache_dir="ncbi_cache",
+    cache_duration_days=cache_days,
+    enabled=True
+).clear_cache(older_than_days=cache_days)
 
 # This applies caching to ALL Entrez calls (esearch and efetch)
 ncbi_cache.apply_cache_to_entrez()
