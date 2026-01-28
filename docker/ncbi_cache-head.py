680bba2c7a7ec25e8042f8cf81c32e75d3795072
Andreas Kähäri <andreas.kahari@nbis.se>
2026-01-28 12:45:23 +0100

    feat: Make cache turnable offable

--- docker/ncbi_cache-head.py
+++ docker/ncbi_cache-head.py
@@ -1,13 +1,15 @@
 import os
 import ncbi_cache
 
-cache_days = int(os.getenv("ENTREZ_CACHE_EXPIRY_DAYS", "3"))
+use_cache = os.getenv("ENTREZ_USE_CACHE", "true").lower() != "false"
+cache_expiry_days = int(os.getenv("ENTREZ_CACHE_EXPIRY_DAYS", "3"))
 
-ncbi_cache.init_cache(
-    cache_dir="ncbi_cache",
-    cache_duration_days=cache_days,
-    enabled=True
-).clear_cache(older_than_days=cache_days)
+if use_cache:
+    ncbi_cache.init_cache(
+        cache_dir="ncbi_cache",
+        cache_duration_days=cache_expiry_days,
+        enabled=True
+    ).clear_cache(older_than_days=cache_expiry_days)
 
-# This applies caching to ALL Entrez calls (esearch and efetch)
-ncbi_cache.apply_cache_to_entrez()
+    # This applies caching to ALL Entrez calls (esearch and efetch)
+    ncbi_cache.apply_cache_to_entrez()
