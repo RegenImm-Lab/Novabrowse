f6b4594448fd537a9ab1429f68dcdc4da44aff2a
Andreas Kähäri <andreas.kahari@nbis.se>
2026-01-29 17:22:59 +0100

    fix: Esu the "diskcache" module instead --> less code

--- docker/ncbi_cache-head.py
+++ docker/ncbi_cache-head.py
@@ -2,14 +2,16 @@ import os
 import ncbi_cache
 
 use_cache = os.getenv("ENTREZ_USE_CACHE", "true").lower() != "false"
-cache_expiry_days = int(os.getenv("ENTREZ_CACHE_EXPIRY_DAYS", "3"))
 
 if use_cache:
+    # Size limit is in megabytes, default to 500 MB.
+    size_limit_mb = int(os.getenv("ENTREZ_CACHE_SIZE_MB", "500"))
+
     ncbi_cache.init_cache(
         cache_dir="ncbi_cache",
-        cache_duration_days=cache_expiry_days,
+        size_limit_mb=size_limit_mb,
         enabled=True
-    ).clear_cache(older_than_days=cache_expiry_days)
+    )
 
     # This applies caching to ALL Entrez calls (esearch and efetch)
     ncbi_cache.apply_cache_to_entrez()
