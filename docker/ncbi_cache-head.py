2f915d6ada34f1ee77e9783042de769632a9aa89
Andreas Kähäri <andreas.kahari@nbis.se>
2026-01-29 17:26:10 +0100

    chore: Ruff reformat

--- docker/ncbi_cache-head.py
+++ docker/ncbi_cache-head.py
@@ -8,9 +8,7 @@ if use_cache:
     size_limit_mb = int(os.getenv("ENTREZ_CACHE_SIZE_MB", "500"))
 
     ncbi_cache.init_cache(
-        cache_dir="ncbi_cache",
-        size_limit_mb=size_limit_mb,
-        enabled=True
+        cache_dir="ncbi_cache", size_limit_mb=size_limit_mb, enabled=True
     )
 
     # This applies caching to ALL Entrez calls (esearch and efetch)
