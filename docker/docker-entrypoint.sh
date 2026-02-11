4dabfd5fc9002f9f0fb5ad71fa2d8bf522eb1a1e
Andreas Kähäri <andreas.kahari@nbis.se>
2026-02-11 16:15:25 +0100

    feat: Partly rewrite and refactor to make simpler to run

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,13 +1,26 @@
 #!/bin/sh -
 
-# This script serves as the entrypoint for the Novabrowse Docker
-# container.  It runs the Novabrowse notebook script and moves the
-# generated HTML files to the appropriate directory.
+set -u
 
-if [ ! -f chromosome_data.json ]; then
-	printf 'chromosome_data.json not found.\n' >&2
-	exit 1
+# Get the UID and GID from the /project/novabrowse_output directory
+OUTPUT_DIR=novabrowse_output
+
+USER_ID=1000
+GROUP_ID=1000
+
+if [ -d "$OUTPUT_DIR" ]; then
+    USER_ID=$(stat -c "%u" "$OUTPUT_DIR")
+    GROUP_ID=$(stat -c "%g" "$OUTPUT_DIR")
+else
+    printf 'Directory "%s" does not exist. Using default UID and GID.\n' "$OUTPUT_DIR" >&2
 fi
+printf 'Using UID: %s and GID: %s\n' "$USER_ID" "$GROUP_ID" >&2
 
-echo 'Running Novabrowse notebook...' 2>&1
-python3 notebook.py && mv Novabrowse*.html novabrowse_output/
+# Change ownership of the NCBI cache directory to the determined UID and
+# GID.
+chown -R "$USER_ID:$GROUP_ID" ncbi_cache/
+
+gosu "$USER_ID:$GROUP_ID" python3 notebook.py >&2
+
+# Copy created output files to the output directory.
+mv Novabrowse_*.html gene_fetch.log "$OUTPUT_DIR"/
